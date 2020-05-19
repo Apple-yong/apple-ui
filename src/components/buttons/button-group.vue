f1bab2a6a164a2c03e23148f2c2ba3c180d31075
Apple-yong <748840808@qq.com>
2020-05-19 18:23:38 +0800

    错误警告

--- src/components/buttons/button-group.vue
+++ src/components/buttons/button-group.vue
@@ -5,7 +5,14 @@
 </template>
 <script>
 export default {
-    
+    mounted() {
+        for(let node of this.$el.children){
+            let name = node.nodeName.toLowerCase()
+            if(name != 'button'){
+                console.warn(`g-button-group的子元素应该是button，而你写的是${name}`)
+            }
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
